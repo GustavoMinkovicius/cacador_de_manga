c299f3a9ff8b4092fa1848b1245267bab3dcbb09
GustavoMinkovicius <87321967+GustavoMinkovicius@users.noreply.github.com>
2021-07-29 15:44:07 -0300

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,28 @@
+class launcher{
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 1
+        }
+        this.bodyA = bodyA;
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+      }
+      display(){
+        if(this.sling.bodyA){
+          var pointA = this.sling.bodyA.position
+          var pointB = this.pointB
+          strokeWeight(2);
+          line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+      }
+      voar(){
+        this.sling.bodyA = null
+      }
+      atacar(bodyA){
+          this.sling.bodyA = bodyA
+      }
+}
